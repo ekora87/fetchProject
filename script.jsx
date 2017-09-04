4dc75957b00b794a74eb250f9f296e1381ee8409
ekora87 <ekora87@gmail.com>
2017-09-04 06:36:37 -0500

    Fetch Project

--- script.jsx
+++ script.jsx
@@ -7,14 +7,40 @@ const styles = require('./style.css');
 class Fetch extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      countries: []
+    };
   }
 
   render() {
+    fetch('http://services.groupkt.com/country/get/all').then((response) => {
+      response.json().then((data) => {
+        const countries = data.RestResponse.result;
+        this.setState({countries});
+      });
+    });
+
     return (
-      <h1>Address Book / Country Codes</h1>
-      // Your code here
+      <div>
+        <h1>Address Book</h1>
+        <table className = "table table-bordered">
+          <tr>
+            <th>#</th>
+            <th>Country Name</th>
+            <th>Alpha2 Code</th>
+            <th>Alpha3 Code</th>
+          </tr>
+          {this.state.countries.map((place, index) => (
+            <tr key={index}>
+              <td className='number'>{index}</td>
+              <td>{place.name}</td>
+              <td>{place.alpha2_code}</td>
+              <td>{place.alpha3_code}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     );
   }
 }
-
 ReactDOM.render(<Fetch />, document.getElementById('fetch'));
